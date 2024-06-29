5d01b073d1e4e314729c9e35324723cab85b8fcd
Lyubov Agulova <lu7623@gmail.com>
2024-06-29 11:36:16 +0300

    feat: add pre-commit hook

--- src/services/filmService.ts
+++ src/services/filmService.ts
@@ -1,8 +1,5 @@
-import { FullMovieInfo, IQueryParams, IResponse, ShortMovieInfo } from '@/api/types';
+import { FullMovieInfo, IQueryParams, IResponse } from '@/api/types';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
-
-
 
 export const filmpoiskAPI = createApi({
   reducerPath: 'filmpoiskAPI',
@@ -10,18 +7,26 @@ export const filmpoiskAPI = createApi({
   endpoints: (builder) => ({
     getFilmsList: builder.query<IResponse, void>({
       query: () => `/search`,
-    //   transformResponse: (resp: { flavor_text_entries: PokemonDesc[] }) =>
-    //     resp.flavor_text_entries
-    //       .filter((item: PokemonDesc) => {
-    //         return item.language.name === 'en';
-    //       })[0]
-    //       .flavor_text.replace(/[^a-zA-Z é . , ']/g, ' '),
-     }),
+      //   transformResponse: (resp: { flavor_text_entries: PokemonDesc[] }) =>
+      //     resp.flavor_text_entries
+      //       .filter((item: PokemonDesc) => {
+      //         return item.language.name === 'en';
+      //       })[0]
+      //       .flavor_text.replace(/[^a-zA-Z é . , ']/g, ' '),
+    }),
     getFilmById: builder.query<FullMovieInfo, string>({
       query: (id: string) => `/movie/${id}`,
     }),
     getFilmsSearch: builder.query<IResponse, IQueryParams>({
-      query: ({ title, release_year, genre, page=1, limit=10, order ='desc', sort_by = 'rating'  }) => ({
+      query: ({
+        title,
+        release_year,
+        genre,
+        page = 1,
+        limit = 10,
+        order = 'desc',
+        sort_by = 'rating',
+      }) => ({
         url: '/search',
         params: {
           title: title,
@@ -30,14 +35,18 @@ export const filmpoiskAPI = createApi({
           order: order,
           limit: limit,
           page: page,
-          sort_by: sort_by
+          sort_by: sort_by,
         },
       }),
-    //   transformResponse: (resp: { results: PokemonType[] }) => resp.results,
-     }),
+      //   transformResponse: (resp: { results: PokemonType[] }) => resp.results,
+    }),
   }),
 });
 
-export const { useGetFilmsListQuery, useGetFilmByIdQuery, useGetFilmsSearchQuery } = filmpoiskAPI; 
- 
+export const {
+  useGetFilmsListQuery,
+  useGetFilmByIdQuery,
+  useGetFilmsSearchQuery,
+} = filmpoiskAPI;
+
 export default filmpoiskAPI;
